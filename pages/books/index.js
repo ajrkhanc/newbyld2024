be1d59402a432d7dc52a0aeea8e3e22b7646a174
ajrkhanc <marketing.aaq@byldgroup.com>
2024-09-13 15:39:13 +0530

    Update
    Update

--- pages/books/index.js
+++ pages/books/index.js
@@ -1,93 +1,179 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Slider from "react-slick";
+// import Head from 'next/head'
+// import Link from 'next/link'
+// import Slider from "react-slick";
 
 
-export default function Books(){
+// export default function Books(){
 
     
 
 
-    return(
+//     return(
+//         <>
+//           <Head>
+//             <title>Books - BYLD Group</title>
+//             <link rel="canonical" href="https://byldgroup.com/books" />
+//             <meta name="description" content="Explore Our Library Crucial Conversations First Chapter Crucial Accountability First Chapter Influencer First Chapter The Power of Habit Book Online Management and Leadership Resources Measuring the Impact and ROI of Leadership Training 7 Ways Poor Leaders Are Costing Your Company Money 3 Fatal Mistakes Managers Make When Coaching Their Teams When It Comes to Customer… Continue reading Books"/> 
+//           </Head>
+            
+//             <div className="rs-about style2 clientcornnerlogo ptt-20 pbb-30">
+//                 <div className="container">
+//                     <div className="row">
+//                         <div className="col-sm-12">
+//                            <div className="">
+//                                <div className="sec-title3 mbb-35">
+//                                     <h4 className="countertoph2 text-center font30">Explore Our Library</h4>
+//                                     <div className="heading-border-line center-style"></div>
+//                                </div>                             
+//                             </div>
+//                         </div>
+                        
+//                         <div className="col-sm-3">
+//                            <div className="innervideoshd">
+//                                <a href='https://payments.byldgroup.com' target='_blank'>
+//                                <img src='/assets/img/books/crucialconversations.svg' alt="books"/>
+//                               <div className='book-details'>
+//                               <div className='title'>Crucial Conversations First Chapter</div>
+//                               </div>
+
+//                               <div className='d-flex justify-content-between align-items-center text-center p-3'>
+//                               <div className='price'>Rs.419.00/-</div>
+//                               <a href='https://payments.byldgroup.com' target='_blank' class="ebookbtn mt-0">Buy Now</a>
+//                               </div>
+//                                </a>
+//                             </div>
+//                         </div>
+
+//                         <div className="col-sm-3">
+//                            <div className="innervideoshd">
+//                                <a href='https://payments.byldgroup.com' target='_blank'>
+//                                <img src='/assets/img/books/crucialaccountability.svg' alt="books"/>
+
+//                                <div className='book-details'>
+//                               <div className='title'>Crucial Accountability First Chapter</div>
+//                               </div>
+
+//                               <div className='d-flex justify-content-between align-items-center text-center p-3'>
+//                               <div className='price'>Rs.642.00/-</div>
+//                               <a href='https://payments.byldgroup.com' target='_blank' class="ebookbtn mt-0">Buy Now</a>
+//                               </div>
+
+                               
+//                                </a>
+//                             </div>
+//                         </div>
+
+//                         <div className="col-sm-3">
+//                            <div className="innervideoshd">
+//                                <a href='https://payments.byldgroup.com' target='_blank'>
+//                                <img src='/assets/img/books/influencer.svg' alt="books"/>
+//                                <div className='book-details'>
+//                               <div className='title'>Influencer First Chapter</div>
+//                               </div>
+
+//                               <div className='d-flex justify-content-between align-items-center text-center p-3'>
+//                               <div className='price'>Rs.742.00/-</div>
+//                               <a href='https://payments.byldgroup.com' target='_blank' class="ebookbtn mt-0">Buy Now</a>
+//                               </div>
+
+                            
+//                                </a>
+//                             </div>
+//                         </div>
+                      
+
+//                     </div>
+//                 </div>                
+//             </div>
+
+
+            
+//         </>
+//     )
+// }
+
+
+
+import Head from 'next/head'
+import { useState, useEffect } from 'react';
+
+export default function Books() {
+    const [prices, setPrices] = useState({
+        crucialConversations: 419,
+        crucialAccountability: 642,
+        influencer: 742
+    });
+
+    return (
         <>
-          <Head>
-            <title>Books - BYLD Group</title>
-            <link rel="canonical" href="https://byldgroup.com/books" />
-            <meta name="description" content="Explore Our Library Crucial Conversations First Chapter Crucial Accountability First Chapter Influencer First Chapter The Power of Habit Book Online Management and Leadership Resources Measuring the Impact and ROI of Leadership Training 7 Ways Poor Leaders Are Costing Your Company Money 3 Fatal Mistakes Managers Make When Coaching Their Teams When It Comes to Customer… Continue reading Books"/> 
-          </Head>
+            <Head>
+                <title>Books - BYLD Group</title>
+                <link rel="canonical" href="https://byldgroup.com/books" />
+                <meta name="description" content="Explore Our Library Crucial Conversations First Chapter Crucial Accountability First Chapter Influencer First Chapter The Power of Habit Book Online Management and Leadership Resources Measuring the Impact and ROI of Leadership Training 7 Ways Poor Leaders Are Costing Your Company Money 3 Fatal Mistakes Managers Make When Coaching Their Teams When It Comes to Customer… Continue reading Books"/> 
+            </Head>
             
             <div className="rs-about style2 clientcornnerlogo ptt-20 pbb-30">
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-12">
-                           <div className="">
-                               <div className="sec-title3 mbb-35">
+                            <div className="">
+                                <div className="sec-title3 mbb-35">
                                     <h4 className="countertoph2 text-center font30">Explore Our Library</h4>
                                     <div className="heading-border-line center-style"></div>
-                               </div>                             
+                                </div>                             
                             </div>
                         </div>
                         
                         <div className="col-sm-3">
-                           <div className="innervideoshd">
-                               <a href='https://payments.byldgroup.com' target='_blank'>
-                               <img src='/assets/img/books/crucialconversations.svg' alt="books"/>
-                              <div className='book-details'>
-                              <div className='title'>Crucial Conversations First Chapter</div>
-                              </div>
+                            <div className="innervideoshd">
+                                <a href={`https://payments.byldgroup.com?amount=${prices.crucialConversations}`} target='_blank'>
+                                    <img src='/assets/img/books/crucialconversations.svg' alt="books"/>
+                                    <div className='book-details'>
+                                        <div className='title'>Crucial Conversations First Chapter</div>
+                                    </div>
 
-                              <div className='d-flex justify-content-between align-items-center text-center p-3'>
-                              <div className='price'>Rs.419.00/-</div>
-                              <a href='https://payments.byldgroup.com' target='_blank' class="ebookbtn mt-0">Buy Now</a>
-                              </div>
-                               </a>
+                                    <div className='d-flex justify-content-between align-items-center text-center p-3'>
+                                        <div className='price'>Rs.{prices.crucialConversations}/-</div>
+                                        <a href={`https://payments.byldgroup.com?amount=${prices.crucialConversations}`} target='_blank' className="ebookbtn mt-0">Buy Now</a>
+                                    </div>
+                                </a>
                             </div>
                         </div>
 
                         <div className="col-sm-3">
-                           <div className="innervideoshd">
-                               <a href='https://payments.byldgroup.com' target='_blank'>
-                               <img src='/assets/img/books/crucialaccountability.svg' alt="books"/>
+                            <div className="innervideoshd">
+                                <a href={`https://payments.byldgroup.com?amount=${prices.crucialAccountability}`} target='_blank'>
+                                    <img src='/assets/img/books/crucialaccountability.svg' alt="books"/>
+                                    <div className='book-details'>
+                                        <div className='title'>Crucial Accountability First Chapter</div>
+                                    </div>
 
-                               <div className='book-details'>
-                              <div className='title'>Crucial Accountability First Chapter</div>
-                              </div>
-
-                              <div className='d-flex justify-content-between align-items-center text-center p-3'>
-                              <div className='price'>Rs.642.00/-</div>
-                              <a href='https://payments.byldgroup.com' target='_blank' class="ebookbtn mt-0">Buy Now</a>
-                              </div>
-
-                               
-                               </a>
+                                    <div className='d-flex justify-content-between align-items-center text-center p-3'>
+                                        <div className='price'>Rs.{prices.crucialAccountability}/-</div>
+                                        <a href={`https://payments.byldgroup.com?amount=${prices.crucialAccountability}`} target='_blank' className="ebookbtn mt-0">Buy Now</a>
+                                    </div>
+                                </a>
                             </div>
                         </div>
 
                         <div className="col-sm-3">
-                           <div className="innervideoshd">
-                               <a href='https://payments.byldgroup.com' target='_blank'>
-                               <img src='/assets/img/books/influencer.svg' alt="books"/>
-                               <div className='book-details'>
-                              <div className='title'>Influencer First Chapter</div>
-                              </div>
+                            <div className="innervideoshd">
+                                <a href={`https://payments.byldgroup.com?amount=${prices.influencer}`} target='_blank'>
+                                    <img src='/assets/img/books/influencer.svg' alt="books"/>
+                                    <div className='book-details'>
+                                        <div className='title'>Influencer First Chapter</div>
+                                    </div>
 
-                              <div className='d-flex justify-content-between align-items-center text-center p-3'>
-                              <div className='price'>Rs.742.00/-</div>
-                              <a href='https://payments.byldgroup.com' target='_blank' class="ebookbtn mt-0">Buy Now</a>
-                              </div>
-
-                            
-                               </a>
+                                    <div className='d-flex justify-content-between align-items-center text-center p-3'>
+                                        <div className='price'>Rs.{prices.influencer}/-</div>
+                                        <a href={`https://payments.byldgroup.com?amount=${prices.influencer}`} target='_blank' className="ebookbtn mt-0">Buy Now</a>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-                      
-
                     </div>
                 </div>                
             </div>
-
-
-            
         </>
-    )
+    );
 }
