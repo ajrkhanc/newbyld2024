643f0e290f292d0e14f2aec15896729cec193d53
ajrkhanc <marketing.aaq@byldgroup.com>
2024-10-01 15:27:04 +0530

    update
    update

--- pages/white-paper/index.js
+++ pages/white-paper/index.js
@@ -1,139 +1,101 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 import Slider from "react-slick";
 
+export default function Books() {
+  return (
+    <>
+      <Head>
+        <title>White Paper - BYLD Group</title>
+        <meta name="description" content="White Paper" />
+      </Head>
 
-export default function Books(){
-
-    
-
-
-    return(
-        <>
-          <Head>
-            <title>White Paper - BYLD Group</title>
-            <meta name="description" content="White Paper"/> 
-          </Head>
-            
-            <div className="rs-about style2 clientcornnerlogo ptt-20 pbb-30">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12">
-                           <div className="">
-                               <div className="sec-title3 mbb-35">
-                                    <h4 className="countertoph2 text-center font30">Explore Our White Paper</h4>
-                                    <div className="heading-border-line center-style"></div>
-                               </div>                             
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4 mb-10">
-                           <div className="innervideoshd ebookst text-center">
-                               <a href='/white-paper/empower-your-dealer-sales-teams-for-the-ev-revolution'>
-                               <img src='/assets/img/white-paper/wp_AmarWhiteprImg.webp' alt="books"/>
-                               <h4 className=''>Empower Your Dealer Sales Teams For The Ev Revolution</h4>
-                               <h3 className='ebookbtn'>Read White Paper</h3>
-                               </a>
-                            </div>
-                        </div>
-                        <div className="col-sm-4 mb-10">
-                           <div className="innervideoshd ebookst text-center">
-                               <a href='/white-paper/total-cost-of-ownership-(tco)-analysis'>
-                               <img src='/assets/img/white-paper/v1_totalImg.webp' alt="books"/>
-                               <h4 className=''>Total cost of Ownership (tco) Analysis</h4>
-                               <h3 className='ebookbtn'>Read White Paper</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4 mb-10">
-                           <div className="innervideoshd ebookst text-center">
-                               <a href='/white-paper/creating-agile-leadership-teams'>
-                               <img src='/assets/img/white-paper/creating-agile-leadership-teams.jpg' alt="books"/>
-                               <h4 className=''>Creating agile leadership teams</h4>
-                               <h3 className='ebookbtn'>Read White Paper</h3>
-                               </a>
-                            </div>
-                        </div>
-                        
-                        <div className="col-sm-4 mb-10">
-                           <div className="innervideoshd ebookst text-center">
-                               <a href='/white-paper/what-it-means-to-be-an-ambidextrous-innovation-leader'>
-                               <img src='/assets/img/white-paper/what-it-means-to-be-an-ambidextrous-innovation-leader.jpg' alt="books"/>
-                               <h4 className=''>What it means to be an ambidextrous innovation leader</h4>
-                               <h3 className='ebookbtn'>Read White Paper</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4 mb-10">
-                           <div className="innervideoshd ebookst text-center">
-                               <a href='/white-paper/alignment-through-purpose-driven-innovation'>
-                               <img src='/assets/img/white-paper/alignment-through-purpose-driven-innovation.jpg' alt="books"/>
-                               <h4 className=''>Alignment for responsible & purpose-driven innovation</h4>
-                               <h3 className='ebookbtn'>Read White Paper</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        {/* <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/agility-unlocked'>
-                               <img src='/edassets/img/ebooki/agility-unlocked.jpg' alt="books"/>
-                               <h4 className=''>Agility Unlocked</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/history-of-disc'>
-                               <img src='/edassets/img/ebooki/history-of-disc.jpg' alt="books"/>
-                               <h4 className=''>History of DiSC</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/the-invisible-drain-on-your-companys-culture'>
-                               <img src='/edassets/img/ebooki/the-invisible-drain-on-your-companys-culture.jpg' alt="books"/>
-                               <h4 className=''>The Invisible Drain on Your Company’s Culture</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/under-the-hood-the-secret-engine-that-drives-destructive-conflict'>
-                               <img src='/edassets/img/ebooki/under-the-hood-the-secret-engine-that-drives-destructive-conflict.jpg' alt="books"/>
-                               <h4 className=''>The Under the Hood</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/virtual-culture-dilemma'>
-                               <img src='/edassets/img/ebooki/virtual-culture-dilemma.jpg' alt="books"/>
-                               <h4 className=''>Tackling the Virtual Culture Dilemma</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div> */}
-
-                    </div>
-                </div>                
+      <div className="rs-about style2 clientcornnerlogo ptt-20 pbb-30">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="">
+                <div className="sec-title3 mbb-35">
+                  <h4 className="countertoph2 text-center font30">
+                    Explore Our White Paper
+                  </h4>
+                  <div className="heading-border-line center-style"></div>
+                </div>
+              </div>
             </div>
 
+            <div className="col-sm-4 mb-10">
+              <div className="innervideoshd ebookst text-center">
+                <a href="/white-paper/empower-your-dealer-sales-teams-for-the-ev-revolution">
+                  <img
+                    src="/assets/img/white-paper/wp_AmarWhiteprImg.webp"
+                    alt="books"
+                  />
+                  <h4 className="">
+                    Empower Your Dealer Sales Teams For The Ev Revolution
+                  </h4>
+                  <h3 className="ebookbtn">Read White Paper</h3>
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-4 mb-10">
+              <div className="innervideoshd ebookst text-center">
+                <a href="/white-paper/total-cost-of-ownership-(tco)-analysis">
+                  <img
+                    src="/assets/img/white-paper/v1_totalImg.webp"
+                    alt="books"
+                  />
+                  <h4 className="">Total cost of Ownership (tco) Analysis</h4>
+                  <h3 className="ebookbtn">Read White Paper</h3>
+                </a>
+              </div>
+            </div>
 
-           
+            <div className="col-sm-4 mb-10">
+              <div className="innervideoshd ebookst text-center">
+                <a href="/white-paper/creating-agile-leadership-teams">
+                  <img
+                    src="/assets/img/white-paper/creating-agile-leadership-teams.webp"
+                    alt="books"
+                  />
+                  <h4 className="">Creating agile leadership teams</h4>
+                  <h3 className="ebookbtn">Read White Paper</h3>
+                </a>
+              </div>
+            </div>
 
-            
-        </>
-    )
+            <div className="col-sm-4 mb-10">
+              <div className="innervideoshd ebookst text-center">
+                <a href="/white-paper/what-it-means-to-be-an-ambidextrous-innovation-leader">
+                  <img
+                    src="/assets/img/white-paper/what-it-means-to-be-an-ambidextrous-innovation-leader.webp"
+                    alt="books"
+                  />
+                  <h4 className="">
+                    What it means to be an ambidextrous innovation leader
+                  </h4>
+                  <h3 className="ebookbtn">Read White Paper</h3>
+                </a>
+              </div>
+            </div>
+
+            <div className="col-sm-4 mb-10">
+              <div className="innervideoshd ebookst text-center">
+                <a href="/white-paper/alignment-through-purpose-driven-innovation">
+                  <img
+                    src="/assets/img/white-paper/alignment-through-purpose-driven-innovation.webp"
+                    alt="books"
+                  />
+                  <h4 className="">
+                    Alignment for responsible & purpose-driven innovation
+                  </h4>
+                  <h3 className="ebookbtn">Read White Paper</h3>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
