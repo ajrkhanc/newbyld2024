2b23bc488d06caa20f86b49d1a51176777a468cc
ajrkhanc <marketing.aaq@byldgroup.com>
2024-10-01 14:48:15 +0530

    update
    update

--- pages/ebook/index.js
+++ pages/ebook/index.js
@@ -30,7 +30,7 @@ export default function Books(){
                         <div className="col-sm-4">
                            <div className="innervideoshd ebookst text-center">
                                <a href='/ebook/critical-skills-every-sales-manager-needs'>
-                               <img src='/assets/img/ebook/critical-skills-every-sales-manager-needs.jpg' alt="books"/>
+                               <img src='/assets/img/ebook/critical-skills-every-sales-manager-needs.webp' alt="books"/>
                                <h4 className=''>Meeting the Quota Challenge: Critical Skills Every Sales Manager Needs to Excel</h4>
                                <h3 className='ebookbtn'>Read E-book</h3>
                                </a>
@@ -40,7 +40,7 @@ export default function Books(){
                         <div className="col-sm-4">
                            <div className="innervideoshd ebookst text-center">
                                <a href='/ebook/2023-hr-and-trends'>
-                               <img src='/assets/img/ebook/2023-hr-and-trends.jpg' alt="books"/>
+                               <img src='/assets/img/ebook/2023-hr-and-trends.jwebp' alt="books"/>
                                <h4 className=''>2023 HR and L&D Trends</h4>
                                <h3 className='ebookbtn'>Read E-book</h3>
                                </a>
@@ -50,62 +50,13 @@ export default function Books(){
                         <div className="col-sm-4">
                            <div className="innervideoshd ebookst text-center">
                                <a href='/ebook/inout-creating-coaching-culture'>
-                               <img src='/assets/img/ebook/inout-creating-coaching-culture.jpg' alt="books"/>
+                               <img src='/assets/img/ebook/inout-creating-coaching-culture.webp' alt="books"/>
                                <h4 className=''>Creating a Coaching Culture</h4>
                                <h3 className='ebookbtn'>Read E-book</h3>
                                </a>
                             </div>
                         </div>
 
-                        {/* <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/agility-unlocked'>
-                               <img src='/edassets/img/ebooki/agility-unlocked.jpg' alt="books"/>
-                               <h4 className=''>Agility Unlocked</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/history-of-disc'>
-                               <img src='/edassets/img/ebooki/history-of-disc.jpg' alt="books"/>
-                               <h4 className=''>History of DiSC</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/the-invisible-drain-on-your-companys-culture'>
-                               <img src='/edassets/img/ebooki/the-invisible-drain-on-your-companys-culture.jpg' alt="books"/>
-                               <h4 className=''>The Invisible Drain on Your Company’s Culture</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/under-the-hood-the-secret-engine-that-drives-destructive-conflict'>
-                               <img src='/edassets/img/ebooki/under-the-hood-the-secret-engine-that-drives-destructive-conflict.jpg' alt="books"/>
-                               <h4 className=''>The Under the Hood</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-4">
-                           <div className="innervideoshd ebookst">
-                               <a href='/everythingdisc/ebook/virtual-culture-dilemma'>
-                               <img src='/edassets/img/ebooki/virtual-culture-dilemma.jpg' alt="books"/>
-                               <h4 className=''>Tackling the Virtual Culture Dilemma</h4>
-                               <h3 className='booktitlerow1'>Read eBook</h3>
-                               </a>
-                            </div>
-                        </div> */}
 
                     </div>
                 </div>                
