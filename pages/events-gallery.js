04e0d6cc1c8e0f90ec87781afe702c2cfb528b63
ajrkhanc <marketing.aaq@byldgroup.com>
2024-10-03 11:47:38 +0530

    update
    update

--- pages/events-gallery.js
+++ pages/events-gallery.js
@@ -4,39 +4,39 @@ import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 
 const images = [
-  '/assets/img/events/DSC02240.webp',
-  '/assets/img/events/7R3A9938.webp',
-  '/assets/img/events/7R3A9926.webp',
-  '/assets/img/events/7R3A9919.webp',
-  '/assets/img/events/7R3A9917.webp',
-  '/assets/img/events/7R3A9913.webp',
-  '/assets/img/events/7R3A9911.webp',
-  '/assets/img/events/7R3A9902.webp',
-  '/assets/img/events/7R3A9839.webp',
-  '/assets/img/events/7R3A9717.webp',
-  '/assets/img/events/7R3A9687.webp',
-  '/assets/img/events/7R3A9686.webp',
-  '/assets/img/events/7R3A9545.webp',
-  '/assets/img/events/7R3A9397.webp',
-  '/assets/img/events/7R3A9245.webp',
-  '/assets/img/events/7R3A9217.webp',
-  '/assets/img/events/7R3A9203.webp',
-  '/assets/img/events/7R3A9188.webp',
-  '/assets/img/events/7R3A9151.webp',
-  '/assets/img/events/7R3A9122.webp',
-  '/assets/img/events/7R3A9089.webp',
-  '/assets/img/events/7R3A9057.webp',
-  '/assets/img/events/7R3A9026.webp',
-  '/assets/img/events/7R3A8995.webp',
-  '/assets/img/events/7R3A8993.webp',
-  '/assets/img/events/7R3A8988.webp',
-  '/assets/img/events/7R3A8986.webp',
-  '/assets/img/events/7R3A8978-scaled.webp',
-  '/assets/img/events/7R3A8978-1.webp',
-  '/assets/img/events/7R3A8803-1.webp',
-  '/assets/img/events/7R3A8780-1.webp',
-  '/assets/img/events/7R3A8463.webp',
-  '/assets/img/events/7R3A8462.webp'
+  '/assets/img/events/eventswebp/DSC02240.webp',
+  '/assets/img/events/eventswebp/7R3A9938.webp',
+  '/assets/img/events/eventswebp/7R3A9926.webp',
+  '/assets/img/events/eventswebp/7R3A9919.webp',
+  '/assets/img/events/eventswebp/7R3A9917.webp',
+  '/assets/img/events/eventswebp/7R3A9913.webp',
+  '/assets/img/events/eventswebp/7R3A9911.webp',
+  '/assets/img/events/eventswebp/7R3A9902.webp',
+  '/assets/img/events/eventswebp/7R3A9839.webp',
+  '/assets/img/events/eventswebp/7R3A9717.webp',
+  '/assets/img/events/eventswebp/7R3A9687.webp',
+  '/assets/img/events/eventswebp/7R3A9686.webp',
+  '/assets/img/events/eventswebp/7R3A9545.webp',
+  '/assets/img/events/eventswebp/7R3A9397.webp',
+  '/assets/img/events/eventswebp/7R3A9245.webp',
+  '/assets/img/events/eventswebp/7R3A9217.webp',
+  '/assets/img/events/eventswebp/7R3A9203.webp',
+  '/assets/img/events/eventswebp/7R3A9188.webp',
+  '/assets/img/events/eventswebp/7R3A9151.webp',
+  '/assets/img/events/eventswebp/7R3A9122.webp',
+  '/assets/img/events/eventswebp/7R3A9089.webp',
+  '/assets/img/events/eventswebp/7R3A9057.webp',
+  '/assets/img/events/eventswebp/7R3A9026.webp',
+  '/assets/img/events/eventswebp/7R3A8995.webp',
+  '/assets/img/events/eventswebp/7R3A8993.webp',
+  '/assets/img/events/eventswebp/7R3A8988.webp',
+  '/assets/img/events/eventswebp/7R3A8986.webp',
+  '/assets/img/events/eventswebp/7R3A8978-scaled.webp',
+  '/assets/img/events/eventswebp/7R3A8978-1.webp',
+  '/assets/img/events/eventswebp/7R3A8803-1.webp',
+  '/assets/img/events/eventswebp/7R3A8780-1.webp',
+  '/assets/img/events/eventswebp/7R3A8463.webp',
+  '/assets/img/events/eventswebp/7R3A8462.webp'
 ];
 
 export default class LightboxExample extends Component {
@@ -97,199 +97,199 @@ export default class LightboxExample extends Component {
                        
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 0, })} src="/assets/img/events/DSC02240.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 0, })} src="/assets/img/events/eventswebp/DSC02240.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 1, })} src="/assets/img/events/7R3A9938.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 1, })} src="/assets/img/events/eventswebp/7R3A9938.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 2, })} src="/assets/img/events/7R3A9926.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 2, })} src="/assets/img/events/eventswebp/7R3A9926.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 3, })} src="/assets/img/events/7R3A9919.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 3, })} src="/assets/img/events/eventswebp/7R3A9919.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 4, })} src="/assets/img/events/7R3A9917.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 4, })} src="/assets/img/events/eventswebp/7R3A9917.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 5, })} src="/assets/img/events/7R3A9913.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 5, })} src="/assets/img/events/eventswebp/7R3A9913.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 6, })} src="/assets/img/events/7R3A9911.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 6, })} src="/assets/img/events/eventswebp/7R3A9911.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 7, })} src="/assets/img/events/7R3A9902.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 7, })} src="/assets/img/events/eventswebp/7R3A9902.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 8, })} src="/assets/img/events/7R3A9839.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 8, })} src="/assets/img/events/eventswebp/7R3A9839.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 9, })} src="/assets/img/events/7R3A9717.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 9, })} src="/assets/img/events/eventswebp/7R3A9717.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 10, })} src="/assets/img/events/7R3A9687.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 10, })} src="/assets/img/events/eventswebp/7R3A9687.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 11, })} src="/assets/img/events/7R3A9686.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 11, })} src="/assets/img/events/eventswebp/7R3A9686.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 12, })} src="/assets/img/events/7R3A9545.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 12, })} src="/assets/img/events/eventswebp/7R3A9545.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 13, })} src="/assets/img/events/7R3A9397.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 13, })} src="/assets/img/events/eventswebp/7R3A9397.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 14, })} src="/assets/img/events/7R3A9245.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 14, })} src="/assets/img/events/eventswebp/7R3A9245.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 15, })} src="/assets/img/events/7R3A9217.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 15, })} src="/assets/img/events/eventswebp/7R3A9217.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 16, })} src="/assets/img/events/7R3A9203.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 16, })} src="/assets/img/events/eventswebp/7R3A9203.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 17, })} src="/assets/img/events/7R3A9188.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 17, })} src="/assets/img/events/eventswebp/7R3A9188.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 18, })} src="/assets/img/events/7R3A9151.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 18, })} src="/assets/img/events/eventswebp/7R3A9151.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 19, })} src="/assets/img/events/7R3A9122.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 19, })} src="/assets/img/events/eventswebp/7R3A9122.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 20, })} src="/assets/img/events/7R3A9089.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 20, })} src="/assets/img/events/eventswebp/7R3A9089.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 21, })} src="/assets/img/events/7R3A9057.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 21, })} src="/assets/img/events/eventswebp/7R3A9057.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 22, })} src="/assets/img/events/7R3A9026.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 22, })} src="/assets/img/events/eventswebp/7R3A9026.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 23, })} src="/assets/img/events/7R3A8995.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 23, })} src="/assets/img/events/eventswebp/7R3A8995.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 24, })} src="/assets/img/events/7R3A8993.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 24, })} src="/assets/img/events/eventswebp/7R3A8993.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 25, })} src="/assets/img/events/7R3A8988.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 25, })} src="/assets/img/events/eventswebp/7R3A8988.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 26, })} src="/assets/img/events/7R3A8986.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 26, })} src="/assets/img/events/eventswebp/7R3A8986.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 27, })} src="/assets/img/events/7R3A8978-scaled.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 27, })} src="/assets/img/events/eventswebp/7R3A8978-scaled.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 28, })} src="/assets/img/events/7R3A8978-1.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 28, })} src="/assets/img/events/eventswebp/7R3A8978-1.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 29, })} src="/assets/img/events/7R3A8803-1.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 29, })} src="/assets/img/events/eventswebp/7R3A8803-1.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 30, })} src="/assets/img/events/7R3A8780-1.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 30, })} src="/assets/img/events/eventswebp/7R3A8780-1.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 31, })} src="/assets/img/events/7R3A8463.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 31, })} src="/assets/img/events/eventswebp/7R3A8463.webp" alt="Awards"/>
                             </div>
                         </div>
 
                         <div className='col-sm-4'>
                             <div className='awardsarea'>
-                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 31, })} src="/assets/img/events/7R3A8462.webp" alt="Awards"/>
+                                <img onClick={() => this.setState({ isOpen: true, photoIndex: 31, })} src="/assets/img/events/eventswebp/7R3A8462.webp" alt="Awards"/>
                             </div>
                         </div>
                     </div>
