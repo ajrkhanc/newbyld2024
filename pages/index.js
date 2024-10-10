376ef3815a3cf2d16c82918b4520dd0f7d82648a
ajrkhanc <marketing.aaq@byldgroup.com>
2024-10-10 15:08:13 +0530

    update
    update

--- pages/index.js
+++ pages/index.js
@@ -450,7 +450,7 @@ export default function Home() {
                         <span>E</span>xperiential Learning
                       </h4>
                       <div className="cicon">
-                        <img
+                        {/* <img
                           className="active"
                           src="https://ik.imagekit.io/byld/BYLD/byldallasests/experiential-learning_q260h2aTw.webp"
                           alt="images"
@@ -461,7 +461,17 @@ export default function Home() {
                           src="https://ik.imagekit.io/byld/BYLD/byldallasests/experiential-learningw_QCXfDVFAYK.webp"
                           alt="images"
                           loading="eager"
+                        /> */}
+                        <Image 
+                         className="active"
+                          src="https://ik.imagekit.io/byld/BYLD/byldallasests/experiential-learning_q260h2aTw.webp"
+                          alt="images"
+                          layout="responsive"
+                          loading="lazy"
+                        
                         />
+
+
                       </div>
                     </div>
                   </a>
