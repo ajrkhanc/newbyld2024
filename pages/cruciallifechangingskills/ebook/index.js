d9714153d57d8ce7d2b72ecb7c0d25685fe329bd
ajrkhanc <marketing.aaq@byldgroup.com>
2024-11-28 10:45:47 +0530

    update
    update

--- pages/cruciallifechangingskills/ebook/index.js
+++ pages/cruciallifechangingskills/ebook/index.js
@@ -16,7 +16,7 @@ export default function Books(){
                      
                       <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/overcoming-workplace-baggage.png" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/overcoming-workplace-baggage.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>Overcoming Workplace Baggage</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/overcoming-workplace-baggage'>Read eBook</a>
                        </div>
@@ -25,7 +25,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/leaders-guide-to-influence.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/leaders-guide-to-influence.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>A Leaders Guide to Influence</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/leaders-guide-to-influence'>Read eBook</a>
                        </div>
@@ -33,7 +33,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/doing-more-with-less.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/doing-more-with-less.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>Doing More with Less</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/doing-more-with-less'>Read eBook</a>
                        </div>
@@ -41,7 +41,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/unafraid.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/unafraid.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>How to Speak up About Things <br></br>that Matter</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/unafraid'>Read eBook</a>
                        </div>
@@ -49,7 +49,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/mini-book-getting-things-done.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/mini-book-getting-things-done.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>Getting Things Done the art of stress free productivity</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/mini-book-getting-things-done'>Read eBook</a>
                        </div>
@@ -57,7 +57,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/el4.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/el4.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>Accountability is missing in educational performance standards</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/accountability-is-missing-in-educational-performance-standards'>Read eBook</a>
                        </div>
@@ -65,7 +65,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/el2.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/el2.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>Crucial conversations<sup>®</sup> for mastering dialogue improves productivity at sprint by 93%</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/crucial-conversations-for-mastering-dialogue-transforms-medical-school-culture'>Read eBook</a>
                        </div>
@@ -73,7 +73,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/el3.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/el3.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>Crucial conversations improves oil rig safety by 55 percent in one year</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/crucial-conversations-improves-oil-rig-safety-by-55-percent-in-one-year'>Read eBook</a>
                        </div>
@@ -81,7 +81,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/el6.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/el6.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>Four Surprising Career Habits of Highly Successful People  </h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/four-surprising-career-habits-of-highly-successful-people'>Read eBook</a>
                        </div>
@@ -89,7 +89,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/beating-employee-burnout.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/beating-employee-burnout.webp" alt="books" />
                             <h6 className='bookprice elerningf'>Beating Employee Burnout</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/beating-employee-burnout'>Read Ebook</a>
                        </div>
@@ -99,7 +99,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/distance-is-destroying-dialogue.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/distance-is-destroying-dialogue.webp" alt="books" />
                             <h6 className='bookprice elerningf'>Distance is Destroying Dialogue</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/distance-is-destroying-dialogue'>Read Ebook</a>
                        </div>
@@ -107,7 +107,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/how-to-lead-virtual-teams.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/how-to-lead-virtual-teams.webp" alt="books" />
                             <h6 className='bookprice elerningf'>How to lead virtual teams</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/how-to-lead-virtual-teams'>Read Ebook</a>
                        </div>
@@ -115,7 +115,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/mind-the-gap.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/mind-the-gap.webp" alt="books" />
                             <h6 className='bookprice elerningf'>Mind the Gap</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/mind-the-gap'>Read Ebook</a>
                        </div>
@@ -123,7 +123,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/open-office-secrets.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/open-office-secrets.webp" alt="books" />
                             <h6 className='bookprice elerningf'>Open Office Secrets</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/open-office-secrets'>Read Ebook</a>
                        </div>
@@ -131,7 +131,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/research-10x-your-influence.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/research-10x-your-influence.webp" alt="books" />
                             <h6 className='bookprice elerningf'>How to 10X Your INFLUENCE</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/research-10x-your-influence'>Read Ebook</a>
                        </div>
@@ -139,7 +139,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/research-eliminating-cultures-of-silence.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/research-eliminating-cultures-of-silence.webp" alt="books" />
                             <h6 className='bookprice elerningf'>Eliminating Cultures of Silence</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/research-eliminating-cultures-of-silence'>Read Ebook</a>
                        </div>
@@ -147,7 +147,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/research-silence-kills.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/research-silence-kills.webp" alt="books" />
                             <h6 className='bookprice elerningf'>Silence Kills</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/research-silence-kills'>Read Ebook</a>
                        </div>
@@ -155,7 +155,7 @@ export default function Books(){
                     
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/the-heart-of-the-matter.jpg" alt="books" />
+                            <img class="center-image mbb-15" src="/classets/img/the-heart-of-the-matter.webp" alt="books" />
                             <h6 className='bookprice elerningf'>The Heart of the Matter</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/the-heart-of-the-matter'>Read eBook</a>
                        </div>
@@ -163,7 +163,7 @@ export default function Books(){
 
                     <div class="col-md-4 workshop-col"> 
                        <div class="bookshd text-center">                            
-                            <img class="center-image mbb-15" src="/classets/img/leaders-guide-to-team-productivity.jpg" alt="books" />          
+                            <img class="center-image mbb-15" src="/classets/img/leaders-guide-to-team-productivity.webp" alt="books" />          
                             <h6 className='bookprice elerningf'>The Leader’s Guide to Team Productivity</h6>                           
                             <a class="booksbtn" href='/cruciallifechangingskills/ebook/leaders-guide-to-team-productivity'>Read eBook</a>
                        </div>
