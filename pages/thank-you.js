e2371831dfa41776470aed1199f5ad3664f864fd
ajrkhanc <marketing.aaq@byldgroup.com>
2025-07-09 17:03:14 +0530

    update
    update

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,34 +1,88 @@
-import Head from "next/dist/shared/lib/head"
+import Head from "next/dist/shared/lib/head";
 
-export default function ThankYou(){
-    return(
-        <>
-        <Head>
-            <title>Thank You Lp - BYLD Group</title>
-            <meta name="description" content="BYLD is a pioneering name in leadership development, corporate training, assessment, and staffing. Founded in 1998, the company is the largest group in South Asia offering HR and business productivity solutions for individuals, teams, and organizations. It has also served 300 of 500 Fortune companies."/> 
-      
-        </Head>
-<noscript>
-      <iframe
-        src="https://www.googletagmanager.com/ns.html?id=GTM-WF8WJWJ"
-        height="0"
-        width="0"
-        style={{ display: "none" }}
-      ></iframe>
-    </noscript>
-        
-    
-          <div className="rs-services  ptt-40 pbb-40">
-            <div className="container">
-                <div className="row y-middle">
-                    <div className="col-lg-12">
-                      <img className="img100" src="/assets/img/thank-you-page.jpg"/>
-                    </div>         
+export default function ThankYou() {
+  return (
+    <>
+      <Head>
+        <title>Thank You Lp - BYLD Group</title>
+        <meta
+          name="description"
+          content="BYLD is a pioneering name in leadership development, corporate training, assessment, and staffing. Founded in 1998, the company is the largest group in South Asia offering HR and business productivity solutions for individuals, teams, and organizations. It has also served 300 of 500 Fortune companies."
+        />
+      </Head>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WF8WJWJ"
+          height="0"
+          width="0"
+          style={{ display: "none" }}
+        ></iframe>
+      </noscript>
+
+      <div className="rs-services ">
+        <div className="container-fluid" style={{ padding: "0" }}>
+          <div className="row y-middle">
+            <div className="col-lg-6">
+              <img
+                className="img100"
+                src="/assets/img/thankYouLeftSideImg.png"
+              />
+            </div>
+            <div className="col-lg-6">
+              <div className="thankyouRightSide">
+                <img
+                  className="thankyouRightSideImg"
+                  src="/assets/img/thankYouRightSideImg.png"
+                />
+                <div className="thankyouRightSideInner">
+                  <p className="mt-0 mb-0" style={{ color: "#FF5200" }}>
+                    We have received your details and our representative shall
+                    get back in 24 working hours
+                  </p>
+                  <p className="mt-2">
+                    Meanwhile, don&rsquo;t forget to follow us on Facebook,
+                    LinkedIn, and Twitter for more updates.
+                  </p>
+                  <div
+                    className="socialIcons"
+                    style={{
+                      display: "flex",
+                      gap: "10px",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <a
+                      href="https://www.facebook.com/BYLDGroup"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src="/assets/img/facebook.png" alt="Facebook" />
+                    </a>
+                    <a
+                      href="https://www.linkedin.com/company/byld-group/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src="/assets/img/linkedin.png" alt="LinkedIn" />
+                    </a>
+                    <a
+                      href="https://twitter.com/BYLDGroup"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src="/assets/img/twitter.png" alt="Twitter" />
+                    </a>
+                  </div>
                 </div>
+              </div>
             </div>
-            </div>
+          </div>
+        </div>
+      </div>
 
-            <div className="container pbb-40">
+      {/* <div className="container pbb-40">
                 <div className="row">
                     <div className="col-md-4">
                         <a href="https://www.facebook.com/BYLDGroup" target="_blank">
@@ -58,7 +112,7 @@ export default function ThankYou(){
                         </a>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </div> */}
+    </>
+  );
 }
