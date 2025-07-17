2141ce7840b9bf9b68986ff4afb58ce9637c60db
ajrkhanc <marketing.aaq@byldgroup.com>
2025-07-17 11:43:33 +0530

    update
    update

---
+++ pages/coaching/coaching-assessments/page.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import React from "react";
+import Link from "next/link";
+
+const CoachingAssessments = () => {
+  return (
+    <>
+      <div className="rs-breadcrumbs webinarsbg">
+        <div className="container">
+          <div className="breadcrumb-container theme1">
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+                <span className="separator">/</span>
+              </li>
+              <li>
+                <Link href="/webinars-and-program-experience">Webinars</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="row">
+            <div className="col-sm-8">
+              <div className="breadcrumbs-inner">
+                <h1 className="page-title">
+                  Register For Your Complimentary <br />
+                  Virtual Events
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CoachingAssessments;
