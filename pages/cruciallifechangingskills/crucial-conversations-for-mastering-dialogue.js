a5eef154900fe59fd6599a366e72da812f9ba89f
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2025-07-29 11:37:38 +0530

    update
    update

--- pages/cruciallifechangingskills/crucial-conversations-for-mastering-dialogue.js
+++ pages/cruciallifechangingskills/crucial-conversations-for-mastering-dialogue.js
@@ -131,7 +131,7 @@ export default function CrucialConversationsforMasteringDialogue() {
                   loading="lazy"
                 />
                 <p>
-                  Crucial Conversations<sub>®</sub> <span className="subTitle">for Mastering Dialogue</span>
+                  Crucial Conversations<sub className="tredMark">®</sub> <span className="subTitle">for Mastering Dialogue</span>
                 </p>
               </div>
               <h1>The Standard in Effective Communication</h1>
