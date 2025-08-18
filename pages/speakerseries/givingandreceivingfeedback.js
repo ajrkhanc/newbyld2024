40a2412d671ac72fe5b2e37f21d528565e830dd4
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2025-08-18 12:01:57 +0530

    update
    update

--- pages/speakerseries/givingandreceivingfeedback.js
+++ pages/speakerseries/givingandreceivingfeedback.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function GivingAndReceivingFeedback() {
   const [showText, setShowText] = useState(false);
