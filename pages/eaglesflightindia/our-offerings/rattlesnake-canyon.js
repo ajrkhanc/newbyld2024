adf9eec8cbbc8c07b549ed64cdadb0d9917adf4f
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2025-08-19 14:33:43 +0530

    update
    update

--- pages/eaglesflightindia/our-offerings/rattlesnake-canyon.js
+++ pages/eaglesflightindia/our-offerings/rattlesnake-canyon.js
@@ -6,138 +6,209 @@ import Slider from "react-slick";
 export default function RattlesnakeCanyon(){
 
 
-    return(
-        <>
-          <Head>
-            <title>Best Virtual and Fun Activities for Employees</title>
-               <link rel="canonical" href="https://byldgroup.com/eaglesflightindia/our-offerings/rattlesnake-canyon"/>
-            <meta name="description" content="If you love a win-win situation, Rattlesnake Canyon for employees is the best. You’ll build effective relationships, through virtual learning."/> 
-          </Head>
+    return (
+      <>
+        <Head>
+          <title>Best Virtual and Fun Activities for Employees</title>
+          <link
+            rel="canonical"
+            href="https://byldgroup.com/eaglesflightindia/our-offerings/rattlesnake-canyon"
+          />
+          <meta
+            name="description"
+            content="If you love a win-win situation, Rattlesnake Canyon for employees is the best. You’ll build effective relationships, through virtual learning."
+          />
+        </Head>
 
-          <div className="rs-services">
-            <div className="container-full">
-                <div className="row y-middle">
-                    <div className="col-lg-12">
-                        <img className="img100" src="/efassets/img/banner/ef-rattlesnake.jpg" alt="Rattlesnake Canyon" />
-                    </div>
+        <div className="rs-services">
+          <div className="container-full">
+            <div className="row y-middle">
+              <div className="col-lg-12">
+                <img
+                  className="img100"
+                  src="/efassets/img/banner/ef-rattlesnake.jpg"
+                  alt="Rattlesnake Canyon"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="rs-about style4 bg21 mtt-60">
+          <div class="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <div class="sec-title3 mbb-35 text-left onlyshd">
+                  <h1 class="countertoph2 text-left font36">
+                    Rattlesnake Canyon{" "}
+                    <sup style={{ fontSize: "11px" }}>TM️</sup>
+                  </h1>
+                  <div class="heading-border-line left-style"></div>
                 </div>
+              </div>
             </div>
-            </div>
-
-            <div class="rs-about style4 bg21 mtt-60">
-            <div class="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <div class="sec-title3 mbb-35 text-left onlyshd">
-                        <h1 class="countertoph2 text-left font36">Rattlesnake Canyon</h1>
-                        <div class="heading-border-line left-style"></div>
-                        </div>
-                     </div>
-
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="sec-title3 mbb-35 text-left onlyshd">
+                  <h4 class="countertoph2 text-left font36">THE EXPERIENCE</h4>
+                  <div class="heading-border-line left-style"></div>
+                  <p class="mtt-40 justify">
+                    The Settlers and Merchants of{" "}
+                    <b>Rattlesnake Canyon™ for employees</b> have been offered a
+                    challenging proposition. The locals have four weeks to
+                    secure all the materials necessary to build a railroad as
+                    well as a camp for workers in{" "}
+                    <b>virtual fun activities for employees.</b>
+                  </p>
+                  <p class="justify">
+                    This fast-paced powerful program forces participants to face
+                    the challenges and rewards of real market scenarios. As the
+                    sights and sounds of the great frontier surround them,
+                    participants maximize profit through a series of buying and
+                    selling transactions where rapid turnover is key.
+                  </p>
+                  <h3 className="fw-bold font20 mb-2">
+                    Boost Sales and Negotiation Skills with This Simulation
+                  </h3>
+                  <div className="btn-part ptt-10 pbb-30">
+                    <Link href="/eaglesflightindia/contact-us">
+                      <a className="readon2">Try the Simulation</a>
+                    </Link>
+                  </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="sec-title3 mbb-35 text-left onlyshd">
-                        <h4 class="countertoph2 text-left font36">THE EXPERIENCE</h4>
-                        <div class="heading-border-line left-style"></div>
-                        <p class="mtt-40 justify">
-                        The Settlers and Merchants of <b>Rattlesnake Canyon™ for employees</b> have been offered a challenging proposition. The locals have four weeks to secure all the materials necessary to build a railroad as well as a camp for workers in <b>virtual fun activities for employees.</b>
-                        </p>
-                        <p class="justify">
-                        This fast-paced powerful program forces participants to face the challenges and rewards of real market scenarios. As the sights and sounds of the great frontier surround them, participants maximize profit through a series of buying and selling transactions where rapid turnover is key.
-                        </p>
-                        </div>
-                    </div>
+              </div>
 
-                    <div className="col-lg-6">
-                    <iframe width="100%" height="380" src="https://www.youtube.com/embed/7IZ9a3uFqJg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                    
+              <div className="col-lg-6">
+                <iframe
+                  width="100%"
+                  height="380"
+                  src="https://www.youtube.com/embed/7IZ9a3uFqJg"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="rs-achievement style1 relative ptt-40 pbb-40">
+          <div className="container">
+            <div className="row rs-vertical-middle">
+              <div className="col-lg-6 md-pt-50 text-center">
+                <div className="">
+                  <img src="/efassets/img/rattlesnake-canyon-logo.png" alt="" />
                 </div>
-            </div>
-            </div>
-
-
-            <div className="rs-achievement style1 relative ptt-40 pbb-40">
-            <div className="container">
-                <div className="row rs-vertical-middle">
-                    <div className="col-lg-6 md-pt-50 text-center">
-                        <div className="">
-                        <img src="/efassets/img/rattlesnake-canyon-logo.png" alt=""/>
-                        </div>
-                        <div class="btn-part ptt-10 pbb-30">
-                            <a class="readon2" target="_blank" href="https://ik.imagekit.io/byld/EF/rattlesnake-canyon_s6D3L8tif.pdf?updatedAt=1708512013290">
-                                 DOWNLOAD BROCHURE
-                                <div class="btn-arrow"></div>
-                            </a>
-                        </div>
+                <div class="btn-part ptt-10 pbb-30">
+                  <a
+                    class="readon2"
+                    target="_blank"
+                    href="https://ik.imagekit.io/byld/EF/rattlesnake-canyon_s6D3L8tif.pdf?updatedAt=1708512013290"
+                  >
+                    DOWNLOAD BROCHURE
+                    <div class="btn-arrow"></div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-6 pr-0">
+                <div className="onlyshd">
+                  <div className="rs-estimate">
+                    <h4 className="countertoph2 text-left font26">OVERVIEW</h4>
+                    <p className="justify">
+                      <b>Rattlesnake Canyon™ for employees</b> focuses on
+                      maximizing sales performance in a market scenario
+                      characterized by competition, rapid pace, and tight
+                      deadlines with virtual fun activities for employees.{" "}
+                      <b>In team building activities,</b> participants learn how
+                      to look beyond their current environment and commit their
+                      resources to the potential of “what could be”. They also
+                      discover the benefits of building effective sales and
+                      distribution partnerships. Participants achieve an
+                      understanding of the throughput of both goods and services
+                      and an appreciation for the value of less tangible
+                      resources such as ideas, information, and trust.
+                    </p>
+                    <h4 className="countertoph2 text-left font26">
+                      THE PRACTICAL DETAILS:
+                    </h4>
+                    <ul className="estimate-info mtt-5">
+                      <li>Groups of 20 to over 1000 participants</li>
+                      <li>
+                        Teams of 2-5 participants in virtual fun activities for
+                        employees
+                      </li>
+                      <li>Half-day sessions of team-building activities</li>
+                      <li>
+                        Suitable for cross-functional and intact teams at all
+                        levels
+                      </li>
+                      <li>
+                        Ideal for use in both a conference and training setting
+                      </li>
+                    </ul>
+                    <h3 className="fw-bold font20 mb-2">
+                      Request a Demo to Explore Real-Time Team Collaboration
+                    </h3>
+                    <div className="btn-part ptt-10 pbb-30">
+                      <Link href="/eaglesflightindia/contact-us">
+                        <a className="readon2">Book a Demo</a>
+                      </Link>
                     </div>
-                    <div className="col-lg-6 pr-0">
-                        <div className='onlyshd'>                        
-                        <div className='rs-estimate'>
-                        <h4 className="countertoph2 text-left font26">OVERVIEW</h4>
-                        <p className='justify'>
-                        <b>Rattlesnake Canyon™ for employees</b> focuses on maximizing sales performance in a market scenario characterized by competition, rapid pace, and tight deadlines with virtual fun activities for employees. <b>In team building activities,</b> participants learn how to look beyond their current environment and commit their resources to the potential of “what could be”. They also discover the benefits of building effective sales and distribution partnerships. Participants achieve an understanding of the throughput of both goods and services and an appreciation for the value of less tangible resources such as ideas, information, and trust.
-                        </p>
-                        <h4 className="countertoph2 text-left font26">THE PRACTICAL DETAILS:</h4>
-                        <ul className="estimate-info mtt-5">
-                            <li>Groups of 20 to over 1000 participants</li>
-                            <li>Teams of 2-5 participants in virtual fun activities for employees</li>
-                            <li>Half-day sessions of team-building activities</li>
-                            <li>Suitable for cross-functional and intact teams at all levels</li>
-                            <li>Ideal for use in both a conference and training setting</li>                            
-                        </ul>
-                        </div>
-                        </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="rs-achievement style1 relative ptt-40 pbb-60">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 pr-0">
+                <div className="onlyshd">
+                  <div className="rs-estimate">
+                    <h4 className="countertoph2 text-left font26">RESULTS</h4>
+                    <ul className="estimate-info mtt-5">
+                      <li>Improve sales productivity</li>
+                      <li>Increase team effectiveness</li>
+                      <li>Enhance sales management skills</li>
+                      <li>Strengthen negotiation skills</li>
+                      <li>Build effective partnerships</li>
+                      <li>Fun team-building activities</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-6 pr-0">
+                <div className="onlyshd">
+                  <div className="rs-estimate">
+                    <h4 className="countertoph2 text-left font26">
+                      KEY LEARNING POINTS
+                    </h4>
+                    <ul className="estimate-info mtt-5">
+                      <li>Understand Current Customer Needs</li>
+                      <li>Demonstrate “Quick and Clear” Communication</li>
+                      <li>Pursue Maximum Profit Potential</li>
+                      <li>Initiate Efficient Processes</li>
+                      <li>Build True Partnerships</li>
+                      <li>Access to fun virtual</li>
+                    </ul>
+                    <h3 className="fw-bold font20 mb-2">
+                      See How Rattlesnake Canyon® Enhances Sales Productivity
+                    </h3>
+                    <div className="btn-part ptt-10 pbb-30">
+                      <Link href="/eaglesflightindia/contact-us">
+                        <a className="readon2">See It in Action</a>
+                      </Link>
                     </div>
-                    
-                </div>                 
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
-
-
-            <div className="rs-achievement style1 relative ptt-40 pbb-60">
-            <div className="container">
-                <div className="row">
-                   
-                    <div className="col-lg-6 pr-0">
-                        <div className='onlyshd'>                        
-                        <div className='rs-estimate'>
-                        <h4 className="countertoph2 text-left font26">RESULTS</h4>
-                        <ul className="estimate-info mtt-5">
-                            <li>Improve sales productivity</li>
-                            <li>Increase team effectiveness</li>
-                            <li>Enhance sales management skills</li>
-                            <li>Strengthen negotiation skills</li>
-                            <li>Build effective partnerships</li>
-                            <li>Fun team-building activities</li>
-                        </ul>
-                        </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-6 pr-0">
-                        <div className='onlyshd'>        
-                        <div className='rs-estimate'>
-                        <h4 className="countertoph2 text-left font26">KEY LEARNING POINTS</h4>
-                        <ul className="estimate-info mtt-5">
-                            <li>Understand Current Customer Needs</li>
-                            <li>Demonstrate “Quick and Clear” Communication</li>
-                            <li>Pursue Maximum Profit Potential</li>
-                            <li>Initiate Efficient Processes</li>
-                            <li>Build True Partnerships</li>
-                            <li>Access to fun virtual</li>                            
-                        </ul>
-                        </div>
-                        </div>
-                    </div>
-                    
-                </div>                 
-            </div>
-            </div>
-
-
-  
-        </>
-    )
+          </div>
+        </div>
+      </>
+    );
 }
