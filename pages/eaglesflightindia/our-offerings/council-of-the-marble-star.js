73c5c744944ee14bc264378f1107f48ceca60ddf
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2025-08-19 14:27:47 +0530

    update
    update

--- pages/eaglesflightindia/our-offerings/council-of-the-marble-star.js
+++ pages/eaglesflightindia/our-offerings/council-of-the-marble-star.js
@@ -6,142 +6,233 @@ import Slider from "react-slick";
 export default function CouncilofTheMarbleStar(){
 
 
-    return(
-        <>
-          <Head>
-            <title>Best Team-Building Business Games - Council of the Marble Star</title>
-         <link rel="canonical"href="https://byldgroup.com/eaglesflightindia/our-offerings/council-of-the-marble-star" />  
-            <meta name="description" content="If you feel hesitant to interact, business team-building activities are your need. Learn how to build effective relationships with people at work."/> 
-          </Head>
+    return (
+      <>
+        <Head>
+          <title>
+            Best Team-Building Business Games - Council of the Marble Star{" "}
+            <sup style={{ fontSize: "11px" }}>TM️</sup>{" "}
+          </title>
+          <link
+            rel="canonical"
+            href="https://byldgroup.com/eaglesflightindia/our-offerings/council-of-the-marble-star"
+          />
+          <meta
+            name="description"
+            content="If you feel hesitant to interact, business team-building activities are your need. Learn how to build effective relationships with people at work."
+          />
+        </Head>
 
-          <div className="rs-services">
-            <div className="container-full">
-                <div className="row y-middle">
-                    <div className="col-lg-12">
-                        <img className="img100" src="/efassets/img/banner/ef-council-of-the-marblestar.jpg" alt="Council of the Marble Star" />
-                    </div>
+        <div className="rs-services">
+          <div className="container-full">
+            <div className="row y-middle">
+              <div className="col-lg-12">
+                <img
+                  className="img100"
+                  src="/efassets/img/banner/ef-council-of-the-marblestar.jpg"
+                  alt="Council of the Marble Star"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="rs-about style4  mtt-60">
+          <div class="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <div class="sec-title3 mbb-35 text-left onlyshd">
+                  <h1 class="countertoph2 text-left font36">
+                    Council of the Marble Star
+                  </h1>
+                  <div class="heading-border-line left-style"></div>
                 </div>
+              </div>
             </div>
-            </div>
-
-            <div class="rs-about style4  mtt-60">
-            <div class="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <div class="sec-title3 mbb-35 text-left onlyshd">
-                        <h1 class="countertoph2 text-left font36">Council of the Marble Star</h1>
-                        <div class="heading-border-line left-style"></div>
-                        </div>
-                     </div>
-
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="sec-title3 mbb-35 text-left onlyshd">
+                  <h4 class="countertoph2 text-left font36">
+                    Best Business Team Building Activities – OVERVIEW
+                  </h4>
+                  <div class="heading-border-line left-style"></div>
+                  <p class="mtt-40 justify">
+                    <b>Council of the Marble Star™</b> highlights the issues
+                    surrounding personal effectiveness. Participants take
+                    responsibility for producing quality results amid
+                    uncertainty, lack of direction, competition, and a changing
+                    environment. While some teams focus solely on results,
+                    others socialize and lose the focus of achieving a
+                    productive outcome. The experience teaches that combining
+                    the skills of being productive with the skills of building
+                    and maintaining relationships creates a competitive edge
+                    that leads to success.
+                  </p>
+                  <h3 className="fw-bold font20 mb-2">
+                    Help Your Teams Manage Change Effectively
+                  </h3>
+                  <div className="btn-part ptt-10 pbb-30">
+                    <Link href="/eaglesflightindia/contact-us">
+                      <a className="readon2">Start the Simulation</a>
+                    </Link>
+                  </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="sec-title3 mbb-35 text-left onlyshd">
-                        <h4 class="countertoph2 text-left font36">Best Business Team Building Activities – OVERVIEW</h4>
-                        <div class="heading-border-line left-style"></div>
-                        <p class="mtt-40 justify">
-                        <b>Council of the Marble Star™</b> highlights the issues surrounding personal effectiveness. Participants take responsibility for producing quality results amid uncertainty, lack of direction, competition, and a changing environment. While some teams focus solely on results, others socialize and lose the focus of achieving a productive outcome. The experience teaches that combining the skills of being productive with the skills of building and maintaining relationships creates a competitive edge that leads to success.
-                        </p>                        
-                        </div>
-                    </div>
+              </div>
 
-                    <div className="col-lg-6">
-                    <iframe width="100%" height="365" src="https://www.youtube.com/embed/e7QYq1u-SFs  " title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                    
+              <div className="col-lg-6">
+                <iframe
+                  width="100%"
+                  height="365"
+                  src="https://www.youtube.com/embed/e7QYq1u-SFs  "
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="rs-achievement style1 relative ptt-40 pbb-40">
+          <div className="container">
+            <div className="row rs-vertical-middle">
+              <div className="col-lg-6 md-pt-50 text-center">
+                <div className="">
+                  <img
+                    src="/efassets/img/council-of-the-marble-star-logo.png"
+                    alt=""
+                  />
                 </div>
-            </div>
-            </div>
-
-
-            <div className="rs-achievement style1 relative ptt-40 pbb-40">
-            <div className="container">
-                <div className="row rs-vertical-middle">
-                    <div className="col-lg-6 md-pt-50 text-center">
-                        <div className="">
-                        <img src="/efassets/img/council-of-the-marble-star-logo.png" alt=""/>
-                        </div>
-                        <div class="btn-part ptt-10 pbb-30">
-                            <a class="readon2" target="_blank" href="https://ik.imagekit.io/byld/EF/2nd_eFYXlmA-R.pdf?updatedAt=1708511288367">
-                                 DOWNLOAD BROCHURE
-                                <div class="btn-arrow"></div>
-                            </a>
-                        </div>
+                <div class="btn-part ptt-10 pbb-30">
+                  <a
+                    class="readon2"
+                    target="_blank"
+                    href="https://ik.imagekit.io/byld/EF/2nd_eFYXlmA-R.pdf?updatedAt=1708511288367"
+                  >
+                    DOWNLOAD BROCHURE
+                    <div class="btn-arrow"></div>
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-6 pr-0">
+                <div className="onlyshd">
+                  <div className="rs-estimate">
+                    <h4 className="countertoph2 text-left font26">
+                      Best Team Building Business Games – THE EXPERIENCE
+                    </h4>
+                    <p className="justify mbb-10">
+                      Share in the discovery of Merlin’s journal which outlines
+                      the key to Arthur’s victorious reign.
+                    </p>
+                    <p className="justify mbb-10">
+                      Participants will have the opportunity to take part in
+                      “The Challenge of Excellence” exactly as it took place
+                      back in the days of Camelot. By combining commercial
+                      capability and cultural skills, Merlin recruited “Elders”
+                      to sit on the <b>Council of the Marble Star™.</b>
+                    </p>
+                    <p className="justify">
+                      They will see how villagers became elite members of King
+                      Arthur’s court by competing, trading, negotiating, and
+                      working together. Experiencing the challenges and rewards
+                      of real win-win negotiation, participants will learn how
+                      to achieve the best results through productive
+                      relationships.
+                    </p>
+                    <h3 className="fw-bold font20 mb-2">
+                      Empower Your Teams with Corporate Culture Simulation
+                    </h3>
+                    <div className="btn-part ptt-10 pbb-30">
+                      <Link href="/eaglesflightindia/contact-us">
+                        <a className="readon2">Explore today</a>
+                      </Link>
                     </div>
-                    <div className="col-lg-6 pr-0">
-                        <div className='onlyshd'>                        
-                        <div className='rs-estimate'>
-                        <h4 className="countertoph2 text-left font26">Best Team Building Business Games – THE EXPERIENCE</h4>
-                        <p className='justify mbb-10'>
-                        Share in the discovery of Merlin’s journal which outlines the key to Arthur’s victorious reign.
-                        </p>
-                        <p className='justify mbb-10'>
-                        Participants will have the opportunity to take part in “The Challenge of Excellence” exactly as it took place back in the days of Camelot. By combining commercial capability and cultural skills, Merlin recruited “Elders” to sit on the <b>Council of the Marble Star™.</b>
-                        </p>
-                        <p className='justify'>
-                        They will see how villagers became elite members of King Arthur’s court by competing, trading, negotiating, and working together. Experiencing the challenges and rewards of real win-win negotiation, participants will learn how to achieve the best results through productive relationships.
-                        </p>
-                        </div>
-                        </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="rs-achievement style1 relative ptt-40 pbb-60">
+          <div className="container">
+            <div className="row rs-vertical-middle">
+              <div className="col-lg-6 pr-0">
+                <div className="onlyshd">
+                  <div className="rs-estimate pbb-65">
+                    <h4 className="countertoph2 text-left font26">
+                      THE PRACTICAL DETAILS:
+                    </h4>
+                    <ul className="estimate-info mtt-5">
+                      <li>Groups of 20 to over 1000 participants</li>
+                      <li>Teams of 2-5 participants</li>
+                      <li>
+                        Half-day sessions of <b>company team building games</b>
+                      </li>
+                      <li>
+                        Suitable for cross-functional and intact teams at all
+                        levels
+                      </li>
+                      <li>
+                        Ideal for use in both conference and training setting
+                      </li>
+                    </ul>
+
+                    <h4 className="countertoph2 text-left font26">RESULTS</h4>
+                    <ul className="estimate-info mtt-5">
+                      <li>Build strong, productive relationships</li>
+                      <li>Learn to empower others</li>
+                      <li>Negotiate successfully</li>
+                      <li>Increase leadership and sales effectiveness</li>
+                      <li>Master and influence culture</li>
+                    </ul>
+                    <h3 className="fw-bold font20 mb-2">
+                      Book a Session to Learn Change Management Strategies
+                    </h3>
+                    <div className="btn-part ptt-10 pbb-30">
+                      <Link href="/eaglesflightindia/contact-us">
+                        <a className="readon2">Book a Session</a>
+                      </Link>
                     </div>
-                    
-                </div>                 
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-6 pr-0">
+                <div className="onlyshd">
+                  <div className="rs-estimate">
+                    <h4 className="countertoph2 text-left font26">
+                      KEY LEARNING POINTS
+                    </h4>
+                    <ul className="estimate-info mtt-5">
+                      <li>
+                        Manage for productivity through change or uncertainty
+                      </li>
+                      <li>Create a culture of company team building games</li>
+                      <li>Understand that perceptions outweigh intentions</li>
+                      <li>
+                        Discover personal habits, approaches, and styles that
+                        may block productivity
+                      </li>
+                      <li>
+                        Master the challenge of building quality relationships,
+                        while obtaining quality results
+                      </li>
+                      <li>
+                        Acquire and apply information to ensure maximum
+                        productivity
+                      </li>
+                      <li>
+                        Create a vision for individual and corporate excellence
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
-
-
-            <div className="rs-achievement style1 relative ptt-40 pbb-60">
-            <div className="container">
-                <div className="row rs-vertical-middle">
-                   
-                    <div className="col-lg-6 pr-0">
-                        <div className='onlyshd'>                        
-                        <div className='rs-estimate pbb-65'>
-                        <h4 className="countertoph2 text-left font26">THE PRACTICAL DETAILS:</h4>
-                        <ul className="estimate-info mtt-5">
-                            <li>Groups of 20 to over 1000 participants</li>
-                            <li>Teams of 2-5 participants</li>
-                            <li>Half-day sessions of <b>company team building games</b></li>
-                            <li>Suitable for cross-functional and intact teams at all levels</li>
-                            <li>Ideal for use in both conference and training setting</li>                            
-                        </ul>
-
-                        <h4 className="countertoph2 text-left font26">RESULTS</h4>
-                        <ul className="estimate-info mtt-5">
-                            <li>Build strong, productive relationships</li>
-                            <li>Learn to empower others</li>
-                            <li>Negotiate successfully</li>
-                            <li>Increase leadership and sales effectiveness</li>
-                            <li>Master and influence culture</li>                            
-                        </ul>
-                        </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-6 pr-0">
-                        <div className='onlyshd'>        
-                        <div className='rs-estimate'>
-                        <h4 className="countertoph2 text-left font26">KEY LEARNING POINTS</h4>
-                        <ul className="estimate-info mtt-5">
-                            <li>Manage for productivity through change or uncertainty</li>
-                            <li>Create a culture of company team building games</li>
-                            <li>Understand that perceptions outweigh intentions</li>
-                            <li>Discover personal habits, approaches, and styles that may block productivity</li>
-                            <li>Master the challenge of building quality relationships, while obtaining quality results</li>
-                            <li>Acquire and apply information to ensure maximum productivity</li>
-                            <li>Create a vision for individual and corporate excellence</li>                            
-                        </ul>
-                        </div>
-                        </div>
-                    </div>
-                    
-                </div>                 
-            </div>
-            </div>
-
-
-  
-        </>
-    )
+          </div>
+        </div>
+      </>
+    );
 }
