0d7f8dc08b3a52f94f40d4c6cc340d792171b8ef
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2025-09-09 12:29:32 +0530

    update
    update

--- pages/events-gallerys.js
+++ pages/events-gallerys.js
@@ -81,10 +81,7 @@ export default function EventsGallerys() {
             <div className="col-sm-12">
               <div className="galleryBox">
                 <div class="gallery-item">
-                  <img
-                    src="https://images.unsplash.com/photo-1539137571959-2fef8104f030"
-                    alt="Gallery item"
-                  />
+                  <img src="/assets/img/new/ysImg.webp" alt="Gallery item" />
                   <div class="caption">
                     <h3>Hawaiian Islands</h3>
                     <p>by Logan Lambert</p>
