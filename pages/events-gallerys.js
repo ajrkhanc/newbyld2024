9dcbc5b5a224b2c32ae0362077609ecabf13e849
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2025-09-09 14:16:25 +0530

    update
    update

--- pages/events-gallerys.js
+++ pages/events-gallerys.js
@@ -87,8 +87,11 @@ export default function EventsGallerys() {
                       <h3>Hawaiian Islands</h3>
                       <p>by Logan Lambert</p>
                     </div>
+                    <div>
+                      <h3>Conclave</h3>
+                    </div>
+                    <div className="overlay"></div>
                   </a>
-                  <div className="overlay"></div>
                 </div>
               </div>
             </div>
