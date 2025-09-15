23dd5894a59789bd3a6e659629f00723388f0007
Ajrkhan <102628339+ajrkhanc@users.noreply.github.com>
2025-09-15 12:13:15 +0530

    update
    update

--- pages/events-gallerys.js
+++ pages/events-gallerys.js
@@ -419,28 +419,30 @@ export default function EventsGallerys() {
           </div>
         </div>
         <div className="container">
-          <div className="row">
-            {visibleKeys.map((eventName, idx) => (
-              <div className="col-lg-4 col-sm-12 mb-2" key={idx}>
-                <div
-                  className="galleryBox"
-                  onClick={() => openLightbox(eventName, 0)} // open first image
-                  style={{ cursor: "pointer" }}
-                >
-                  <div className="gallery-item">
-                    <img src={galleries[eventName][0]} alt={eventName} />
-                    <div className="caption">
-                      <h3>{eventName}</h3>
-                    </div>
-                    <div className="overlay">
-                      <div className="overlay-text">
+          <div className={`extra-cards ${showAll ? "expanded" : ""}`}>
+            <div className="row">
+              {visibleKeys.map((eventName, idx) => (
+                <div className="col-lg-4 col-sm-12 mb-2" key={idx}>
+                  <div
+                    className="galleryBox"
+                    onClick={() => openLightbox(eventName, 0)} // open first image
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div className="gallery-item">
+                      <img src={galleries[eventName][0]} alt={eventName} />
+                      <div className="caption">
                         <h3>{eventName}</h3>
+                      </div>
+                      <div className="overlay">
+                        <div className="overlay-text">
+                          <h3>{eventName}</h3>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
 
           {/* ✅ View More / View Less button */}
@@ -475,6 +477,17 @@ export default function EventsGallerys() {
           />
         )}
       </section>
+
+      <style jsx>{`
+        .extra-cards {
+          max-height: 0;
+          overflow: hidden;
+          transition: max-height 0.6s ease-in-out;
+        }
+        .extra-cards.expanded {
+          max-height: 2000px; /* big enough for all cards */
+        }
+      `}</style>
     </>
   );
 }
